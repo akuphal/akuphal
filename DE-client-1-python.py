c445bab4762d7dc78b3f3939980998f56f1dcd92
akuphal <71189982+akuphal@users.noreply.github.com>
2023-03-11 21:02:47 -0600

    Update DE-client-1-python.py

--- DE-client-1-python.py
+++ DE-client-1-python.py
@@ -1,8 +1,3 @@
-###############################################################################
-# client-python.py
-# Name: Tachun Lin
-###############################################################################
-
 import sys
 import socket
 
@@ -11,7 +6,7 @@ RECV_BUFFER_SIZE = 2048
 
 def client(router_ip, router_port, server_ip, server_port):
     client_ip = server_ip
-    client_port = 1220 ## dunno what the client port should be but we have to include in message sent...
+    client_port = 1220
     
     # create an INET, STREAMing socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -33,7 +28,7 @@ def client(router_ip, router_port, server_ip, server_port):
                 """TODO: WAIT FOR SERVER RESPONSE, THEN EXIT"""
                 data = s.recv(RECV_BUFFER_SIZE)
                 if not data: break
-                sys.stdout.buffer.raw.write(data) ##may only write partial messages to output
+                sys.stdout.buffer.raw.write(data)
                 sys.exit("Client exited")
             sys.stdout.flush()
     pass
@@ -43,7 +38,7 @@ def main():
     """TODO: CHANGE TO 4 ARGS: SERVER IP, SERVER PORT, ROUTER IP, ROUTER PORT """
     if len(sys.argv) != 5:
         sys.exit("Usage: python3 client-python.py [Server IP] [Server Port] [Router IP] [Router Port] < [message]")
-    server_ip = sys.argv[1]  ##why do they not start at index 0? imma roll with it
+    server_ip = sys.argv[1]
     server_port = int(sys.argv[2])
     router_ip = sys.argv[3]
     router_port = int(sys.argv[4])
