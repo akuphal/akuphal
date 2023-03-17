d339671f65fc9304a07ffa7db1745d1f2b43887d
akuphal <71189982+akuphal@users.noreply.github.com>
2023-03-16 19:29:36 -0600

    Update DE - TeamWeaknessMatrix.sql

--- DE - TeamWeaknessMatrix.sql
+++ DE - TeamWeaknessMatrix.sql
@@ -1,1 +1,3 @@
-select * from myTeam LEFT JOIN types on myTeam.Type_1 = types.Attacking
+select * from myTeam JOIN types on myTeam.Type_1 = types.Attacking
+union
+select * from myTeam JOIN types on myTeam.Type_2 = types.Attacking
